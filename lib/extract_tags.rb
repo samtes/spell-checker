d42627ce1fb8ec0ad02c7b7fbc5c679f3c131a45
samtes <sweldemariam@gmail.com>
2014-08-28 14:19:35 -0700

    Update extract_tags.rb

--- lib/extract_tags.rb
+++ lib/extract_tags.rb
@@ -7,24 +7,28 @@ class Grab_text
 
   def initialize(file)
     @file = file
-  end
-
-  def check_auth
-   response = Unirest.get @file
-   response.code
-  end
+  end 
+  
+  def print_html
+    page = Nokogiri::HTML(open(@file))
+    page
+  end 
+  
+  def check_auth(username='', password='')
+    response = Unirest.get @file, auth:{:user=>username, :password=>password}
+    response.code
+  end 
 
   def print_auth_page(username, password)
-    (open(@file, :http_basic_authentication => [username, password]))
     page = Nokogiri::HTML(open(@file, :http_basic_authentication => [username, password]))
     page.css('script').remove()
     page.css('body').text
-  end
+  end 
 
   def print_text
     page = Nokogiri::HTML(open(@file))
     page.css('script').remove()
     page.css('body').text
-  end
+  end 
   
 end
